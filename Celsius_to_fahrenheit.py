85818993936e526204744e5ce02a771321354a7a
Ying Xuan Hu <hudaman@Yings-MacBook-Pro.local>
2020-04-01 15:24:38 -0400

    corrected spelling

--- Celsius_to_fahrenheit.py
+++ Celsius_to_fahrenheit.py
@@ -1,10 +1,10 @@
 #Prompt the user for a number in degrees Celsius and convert 
 # the value to degrees in Fahrenheit and display it to the user 
-tmp = int(input ('Please Enter Temperature in C'))
+tmp = int(input ('Please Enter Temperature in C '))
 tmp = int(tmp)
 fah = (tmp *9/5)+32
 fah = str(fah)
-print ('Celsius to Fahrenheit,' + fah )
+print ('Celsius to Fahrenheit Temperature is ' + fah + ' F')
 #output = f'you entered Celsius = {tmp()}'
 #print (output)
 #output = f'Fahrenheit is, {int(fah())}' 
